7ec388ca24205c1452f7e625fdd8927fefe2ea79
yeeric01 <yeeric01@gmail.com>
2020-12-18 21:38:11 -0600

    Added more logic to etch a sketch

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-let divs = document.querySelectorAll(".blocks");
-let clear = document.querySelector("button");
+let clear = document.querySelector(".button1");
+let setGrid = document.querySelector(".button2");
+let centerThis = document.querySelector(".centerThis");
+let board;
+let gridSize = 0;
+let divs = document.querySelectorAll("blocks");;
 
-clear.addEventListener("click", ()=>{
+setGrid.addEventListener("click", ()=>{
+    if(board)
+        centerThis.removeChild(board);
+    board = document.createElement("div");
+    board.classList.add("board");
+    centerThis.appendChild(board);
+    let str = "";
+
+    
+    gridSize = prompt("Enter Grid Size from 1 to 100");
+    if(gridSize > 100)
+        gridSize = 100;
+    for(let i = 0; i < gridSize; i++){str=str+"1fr ";}
+    board.style.setProperty("grid-template-columns", str);
+
+    let tmp = gridSize * gridSize;
+    for(let i = 0; i < tmp; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("blocks");
+        board.appendChild(newDiv);
+    }
+   
+    divs = document.querySelectorAll(".blocks");
+    console.log(divs.length);
     for(let i = 0; i < divs.length; i++){
+        divs[i].addEventListener("mousedown", changeColor1);
+        divs[i].addEventListener("mousemove", changeColor2);
+        divs[i].addEventListener("mouseup", changeColor3);
         divs[i].style.backgroundColor="white";
     }
 });
+clear.addEventListener("click", ()=>{
+    centerThis.removeChild(board);
+});
 
 let isTrue = false;
-let startingColor = "white";
-for(let i = 0; i < divs.length; i++){
-    divs[i].addEventListener("mousedown", changeColor1);
-    divs[i].addEventListener("mousemove", changeColor2);
-    divs[i].addEventListener("mouseup", changeColor3);
-    divs[i].style.backgroundColor="white";
-}
+let startingColor;
 function changeColor1(e){
     isTrue = true;
     startingColor = e.target.style.backgroundColor;
-    if(color =="white")
+    if(startingColor =="white")
         e.target.style.backgroundColor="black";
     else
         e.target.style.backgroundColor="white"
@@ -26,7 +53,6 @@ function changeColor1(e){
 function changeColor2(e){
     if(!isTrue)
         return;
-    console.log(isTrue);
     if(startingColor =="white")
         e.target.style.backgroundColor="black";
     else
@@ -35,7 +61,6 @@ function changeColor2(e){
 function changeColor3(e){
     if(!isTrue)
         return;
-    let color = e.target.style.backgroundColor;
     if(startingColor =="white")
         e.target.style.backgroundColor="black";
     else
